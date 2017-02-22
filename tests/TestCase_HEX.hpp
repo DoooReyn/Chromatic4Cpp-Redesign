4f911130412cc075e77465f0ff2cdc94d41138fe
Reyn <jl88744653@gmail.com>
2017-02-22 23:53:54 +0800

    添加测试用例
    HEX，十六进制数据结构和类测试

---
+++ tests/TestCase_HEX.hpp
@@ -0,0 +1,14 @@
+//
+//  TestCase_HEX.hpp
+//  Playground
+//
+//  Created by Reyn-Mac on 2017/2/21.
+//  Copyright © 2017年 Reyn-Mac. All rights reserved.
+//
+
+#ifndef TestCase_HEX_hpp
+#define TestCase_HEX_hpp
+
+void TestCase1_HEX_ConvertHex();
+
+#endif /* TestCase_HEX_hpp */
---
+++ tests/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chromatic4Cpp-Redesign
+//
+//  Created by Reyn-Mac on 2017/2/22.
+//  Copyright © 2017年 Reyn-Mac. All rights reserved.
+//
+
+#include <iostream>
+#include "TestCase_HEX.hpp"
+
+int main(int argc, const char * argv[]) {
+    TestCase1_HEX_ConvertHex();
+}
