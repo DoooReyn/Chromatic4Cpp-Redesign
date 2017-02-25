1124ab2e23fbad55cf1b4f6a69b14b3f417411eb
Reyn <jl88744653@gmail.com>
2017-02-25 21:39:27 +0800

    Add files via upload

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,13 @@
 //
 //  main.cpp
-//  Chromatic4Cpp-Redesign
+//  SomethingMustBeWrong
 //
-//  Created by Reyn-Mac on 2017/2/22.
+//  Created by Reyn-Mac on 2017/2/25.
 //  Copyright © 2017年 Reyn-Mac. All rights reserved.
 //
 
-#include <iostream>
-#include "TestCase_HEX.hpp"
-#include "TestCase_sRGB.hpp"
+#import "TestCase_HEX.hpp"
+#import "TestCase_sRGB.hpp"
 
 int main(int argc, const char * argv[]) {
     TestCases_HEX();
