e3723b34c541dec0005f7b9a42a107134aa5fb46
Reyn <jl88744653@gmail.com>
2017-02-25 23:13:43 +0800

    增加RGBA测试用例

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,10 +6,12 @@
 //  Copyright © 2017年 Reyn-Mac. All rights reserved.
 //
 
-#import "TestCase_HEX.hpp"
-#import "TestCase_sRGB.hpp"
+#include "TestCase_HEX.hpp"
+#include "TestCase_sRGB.hpp"
+#include "TestCase_sRGBA.hpp"
 
 int main(int argc, const char * argv[]) {
     TestCases_HEX();
     TestCases_sRGB();
+    TestCases_sRGBA();
 }
